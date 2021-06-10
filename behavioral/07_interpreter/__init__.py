1343a50beee25650babcfc893e0c4324f6b2a2a8
Meirkhan_Yesseyev <yesseyev.mt@gmail.com>
2021-06-10 17:30:59 +0600

    [ADD] Interpreter pattern added

---
+++ behavioral/07_interpreter/__init__.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractExpression(ABC):
+	@abstractmethod
+	def interpret(self):
+		pass
+
+
+class NonterminalExpression(AbstractExpression):
+
+	def __init__(self, expression):
+		self._expression = expression
+
+	def interpret(self):
+		print("Non-terminal expression being interpreted ...")
+		self._expression.interpret()
+
+
+class TerminalExpression(AbstractExpression):
+
+	def interpret(self):
+		print("Terminal expression being interpreted ...")
+
+
+def main():
+	ast = NonterminalExpression(NonterminalExpression(TerminalExpression()))
+	ast.interpret()
+
+
+if __name__ == "__main__":
+	main()
